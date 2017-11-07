20e216edd2edd0b2d25200b637ee2ba9d612d985
Laszlo Fodor <LaszloFodor@users.noreply.github.com>
2017-11-07 21:11:05 +0100

    user

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -19,11 +19,19 @@ public class User extends BaseEntity{
     @Column(nullable = false, unique = true)
     private String username;
 
+    @Column(nullable = false)
+    private String password;
+
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
-    private String password;
+    @Column(length = 40, nullable = false)
+    private String firstName;
+    
+    @Column(length = 40, nullable = false)
+    private String lastName;
+
+    private String token;
 
     @Column(nullable = false, unique = true)
     @Enumerated(EnumType.STRING)
