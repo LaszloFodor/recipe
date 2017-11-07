881bca042ed75a765564d262f1c89fe615aa9404
Laszlo Fodor <LaszloFodor@users.noreply.github.com>
2017-11-07 21:49:01 +0100

    usercontroller

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,11 +1,13 @@
 package controller;
 
-
+import model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
@@ -14,9 +16,17 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @GetMapping("/greet")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "User") String name, Model model){
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+
+
     @GetMapping("/login")
     public String login(@ModelAttribute User user, Model model) {
-        if (userService.isValid(User)) {
+        if (userService.isValid(user)) {
             return redirectToGreeting(user);
         }
         model.addAttribute("loginFailed", true);
